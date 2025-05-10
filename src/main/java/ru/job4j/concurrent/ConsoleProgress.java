5e0194c09ca9488beaebdff90a7aafeee733c73d
exsnipe <exsnipe@yandex.ru>
2025-05-10 11:09:36 +0300

    5. Прерывание нити [#1019]

---
+++ src/main/java/ru/job4j/concurrent/ConsoleProgress.java
@@ -0,0 +1,31 @@
+package ru.job4j.concurrent;
+
+public class ConsoleProgress implements Runnable {
+
+    private final static char[] sequence = new char[] {'-', '\\', '|', '/'};
+
+    @Override
+    public void run() {
+        int count = 0;
+        while (!Thread.currentThread().isInterrupted()) {
+            System.out.print("\rLoad " +sequence[count++]);
+            count = count == 4 ? 0 : count;
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Thread thread = new Thread(new ConsoleProgress());
+        thread.start();
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException ex) {
+            ex.printStackTrace();
+        }
+        thread.interrupt();
+    }
+}
